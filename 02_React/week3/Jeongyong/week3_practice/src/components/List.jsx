643c8b4e3fedc257163a229cdbb15342cf9c0019
unknown <jylee2670@gmail.com>
2026-01-04 12:51:55 +0900

    ✅ Update week 7 practice

---
+++ 02_React/week3/Jeongyong/week3_practice/src/components/List.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
+
+function List({ todos, onUpdate, onDelete }) {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") return todos;
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+  const filteredTodos = getFilteredData();
+  return (
+    <div className="List">
+      <h4>Todo List 🎄</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+              key={todo.id}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default List;
